4acbc138eb1c892c8a3e3109f429fd26492aa4a7
Daniil Vaskin <Vaskin.Daniil@wb.ru>
2025-05-12 18:36:05 +0300

    After fix

--- src/test/java/com/example/jetbrainstest/tests/CodeCanvasTest.java
+++ src/test/java/com/example/jetbrainstest/tests/CodeCanvasTest.java
@@ -3,12 +3,11 @@ package com.example.jetbrainstest.tests;
 import com.example.jetbrainstest.pages.CodeCanvasPage;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-
-import static com.example.jetbrainstest.tests.BaseTest.getDriver;
 
 public class CodeCanvasTest {
     private WebDriver driver;
@@ -16,7 +15,7 @@ public class CodeCanvasTest {
 
     @BeforeEach
     public void setUp() {
-        driver = getDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.get("https://www.jetbrains.com/codecanvas/");
@@ -26,7 +25,9 @@ public class CodeCanvasTest {
 
     @AfterEach
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
@@ -44,8 +45,9 @@ public class CodeCanvasTest {
     @Test
     @DisplayName("Переход на страницу новостей с ожиданием URL")
     public void openNewsAndCheckUrl() {
+        canvasPage.clickOnBlog();
         canvasPage.clickOnNews();
-        String expectedUrl = "https://www.jetbrains.com/codecanvas/category/news/";
+        String expectedUrl = "https://blog.jetbrains.com/codecanvas/category/news/";
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.urlToBe(expectedUrl));
         String actualUrl = driver.getCurrentUrl();
